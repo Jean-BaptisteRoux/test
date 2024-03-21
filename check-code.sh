98e18d05e63ef0b4ef7f875554027c52b7ad12a0
Pysannelis <jeanbaptiste.roux@gmail.com>
2024-03-21 14:44:31 +0100

    p

--- check-code.sh
+++ check-code.sh
@@ -3,8 +3,8 @@
 # Vérifier que le fichier n'est pas vide
 if [ -s "$1" ]; then
     echo "Le fichier '$1' n'est pas vide."
+    exit 3
 else
     echo "Erreur : le fichier '$1' est vide."
-    exit 3
 fi
 
